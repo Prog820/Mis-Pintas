0f99b5d1d172e2854717fec23f1bcb625c33126e
StarMar <marirodriguezospina8@gmail.com>
2026-07-01 18:46:21 -0500

    feat: configuración PWA con vite-plugin-pwa

--- mis-pintas/vite.config.js
+++ mis-pintas/vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Mis Pintas',
+        short_name: 'Mis Pintas',
+        description: 'Tu armario virtual',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#060810',
+        theme_color: '#060810',
+        orientation: 'portrait-primary',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+  ],
 })
